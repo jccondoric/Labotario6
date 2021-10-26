f08ded72bd2c84da5369d06420a19819f4aa1679
Jhon Carlos Condori Cuevas <www.hackdcjcc@hotmail.com>
2021-10-26 18:41:07 -0400

    Agregar archivos de proyecto.

---
+++ PBomberManUSFX/GameObject.cpp
@@ -0,0 +1,21 @@
+#include "GameObject.h"
+#include<map>
+
+int GameObject::numeroGameObjectCreados = 0;
+
+GameObject::GameObject() {
+	eliminar = false;
+	numeroGameObjectCreados++;
+	idGameObject = numeroGameObjectCreados;
+}
+
+
+
+
+GameObject::GameObject(string _nombre)
+{
+	eliminar = false;
+	nombre = _nombre;
+	numeroGameObjectCreados++;
+	idGameObject = numeroGameObjectCreados;
+}
